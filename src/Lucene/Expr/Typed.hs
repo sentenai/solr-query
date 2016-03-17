4246433ab22b26c25df640d4a5f4bbe605b3ab47
Mitchell Rosen <mitchellwrosen@gmail.com>
2016-03-17 14:33:14 -0700

    Fill out query API and work on some documentation

--- src/Lucene/Expr/Typed.hs
+++ src/Lucene/Expr/Typed.hs
@@ -10,22 +10,17 @@ module Lucene.Expr.Typed where
 
 import Lucene.Type
 
-import Data.ByteString.Builder (Builder)
-import Data.ByteString.Lazy.Char8    (ByteString)
+import Data.ByteString.Builder    (Builder)
+import Data.ByteString.Lazy.Char8 (ByteString)
 import Data.Monoid
-import Data.String             (IsString(..))
-import Data.Text               (Text)
-import GHC.Exts                (IsList(..))
+import Data.String                (IsString(..))
+import Data.Text                  (Text)
+import GHC.Exts                   (IsList(..))
 
-import qualified Data.Text               as T
-import qualified Data.Text.Encoding      as T
-import qualified Data.ByteString.Builder as BS
-import qualified Data.ByteString.Lazy.Char8    as BS
-
-
-data Ranged a
-  = Inclusive a
-  | Exclusive a
+import qualified Data.Text                  as T
+import qualified Data.Text.Encoding         as T
+import qualified Data.ByteString.Builder    as BS
+import qualified Data.ByteString.Lazy.Char8 as BS
 
 
 data LuceneExpr :: LuceneType -> * where
@@ -58,23 +53,63 @@ instance IsList (LuceneExpr TPhrase) where
   toList (EPhrase ws) = ws
 
 
+data Ranged a
+  = Inclusive a
+  | Exclusive a
+
+
+-- | An @int@ expression.
+--
+-- Note that sometimes you may use the 'Num' instance for 'LuceneExpr' 'TInt',
+-- however, due to the interaction between type classes and GADTs, an explicit
+-- type signature will usually be required.
+--
+-- @
+-- -- foo:5
+-- query :: 'LuceneQuery'
+-- query = "foo" '=:' 'int' 5
+-- @
+--
+-- Here's an example where only one integer has to be explicitly "wrapped" with
+-- 'int', for better or for worse:
+--
+-- @
+-- -- foo:[5 TO 6]
+-- query :: 'LuceneQuery'
+-- query = "foo" '=:' 'incl' ('int' 5) `'to'` 'incl' 6
+-- @
 int :: Int -> LuceneExpr TInt
 int = EInt
 
+-- | A @true@ expression.
+--
+-- @
+-- -- foo:true
+-- query :: 'LuceneQuery'
+-- query = "foo" '=:' 'true'
+-- @
 true :: LuceneExpr TBool
 true = ETrue
 
+-- | A @false@ expression.
+--
+-- @
+-- -- foo:false
+-- query :: 'LuceneQuery'
+-- query = "foo" '=:' 'false'
+-- @
 false :: LuceneExpr TBool
 false = EFalse
-
 
 -- | A single word. Should *not* contain any spaces, wildcard characters (@?@
 -- and @*@), or tildes (@~@), though this is not enforced by the type system.
 --
--- It may be more convenient to use the 'IsString' instance of 'LuceneExpr'
--- 'TWord', which requires the @OverloadedStrings@ language extension.
+-- Note that sometimes you may use the 'IsString' instance for 'LuceneExpr'
+-- 'TWord', however, due to the interaction between type classes and GADTs, an
+-- explicit type signature will usually be required.
 --
 -- @
+-- -- foo:bar
 -- query :: 'LuceneQuery'
 -- query = "foo" '=:' 'word' "bar"
 -- @
@@ -82,8 +117,9 @@ false = EFalse
 -- Or, with @OverloadedStrings@:
 --
 -- @
+-- -- foo:bar
 -- query :: 'LuceneQuery'
--- query = "foo" '=:' "bar"
+-- query = "foo" '=:' ("bar" :: 'LuceneExpr' 'TWord')
 -- @
 word :: Text -> LuceneExpr TWord
 word = EWord
@@ -93,12 +129,24 @@ word = EWord
 -- tildes (@~@), though this is not enforced by the type system.
 --
 -- @
+-- -- foo:b?r
 -- query :: 'LuceneQuery'
 -- query = "foo" '=:' 'wild' "b?r"
 -- @
 wild :: Text -> LuceneExpr TWild
 wild = EWild
 
+-- | A regular expression, whose syntax is described by
+-- 'http://lucene.apache.org/core/5_5_0/core/org/apache/lucene/util/automaton/RegExp.html?is-external=true'.
+--
+-- Note that the leading and trailing @/@ must be omitted. The regex innards are
+-- not type checked in any way.
+--
+-- @
+-- -- foo:/[mb]oat/
+-- query :: 'LuceneQuery'
+-- query = 'regex' "[mb]oat"
+-- @
 regex :: Text -> LuceneExpr TRegex
 regex = ERegex
 
@@ -106,49 +154,107 @@ regex = ERegex
 -- wildcard characters. Both of these properties are enforced by the type
 -- system, as long as the words themselves adhere to the 'word' contract.
 --
--- It may be more convenient to use the 'IsList' instance of 'LuceneExpr'
--- 'TPhrase', which requires the @OverloadedLists@ language extension.
+-- Note that sometimes you may use the 'IsList instance for 'LuceneExpr'
+-- 'TPhrase', however, due to the interaction between type classes and GADTs, an
+-- explicit type signature will usually be required.
+--
+-- With @OverloadedStrings@:
 --
 -- @
+-- -- foo:"bar baz"
 -- query :: 'LuceneQuery'
--- query = "foo" '=:' 'phrase' ['word' "bar", 'word' "baz"] -- ok
+-- query = "foo" '=:' 'phrase' ["bar", "baz"] -- ok
 --
+-- -- foo:"bar b?z" (an invalid Lucene query)
 -- query :: 'LuceneQuery'
--- query = "foo" '=:' 'phrase' ['word' "bar", 'wild' "b?z"] -- type error
+-- query = "foo" '=:' 'phrase' ["bar", 'wild' "b?z"] -- type error
 --
+-- -- foo:"bar b?z" (an invalid Lucene query)
 -- query :: 'LuceneQuery'
--- query = "foo" '=:' 'phrase' ['word' "bar", 'word' "b?z"] -- breaks 'word' contract
+-- query = "foo" '=:' 'phrase' ["bar", "b?z"] -- breaks 'word' contract
 -- @
 --
 -- Or, with @OverloadedLists@ and @OverloadedStrings@:
 --
 -- @
+-- -- foo:"bar baz"
 -- query :: 'LuceneQuery'
--- query = "foo" '=:' ["bar", "baz"] -- ok
---
--- query :: 'LuceneQuery'
--- query = "foo" '=:' ["bar", "b?z"] -- breaks 'word' contract
+-- query = "foo" '=:' (["bar", "baz"] :: 'LuceneExpr' 'TPhrase')
 -- @
 phrase :: [LuceneExpr TWord] -> LuceneExpr TPhrase
 phrase = EPhrase
 
 
-
+-- | A @~@ operator, which fuzzes its argument (either a word or phrase).
+--
+-- This will have one of the following two types:
+--
+-- @
+-- (~:) :: 'LuceneExpr' 'TWord'   -> Int -> 'LuceneExpr' 'TFuzzyWord'
+-- (~:) :: 'LuceneExpr' 'TPhrase' -> Int -> 'LuceneExpr' 'TFuzzyPhrase'
+-- @
+--
+-- A word can only be fuzzed by @0@, @1@, or @2@. A phrase can be fuzzed by any
+-- positive integer.
+--
+-- @
+-- -- foo:bar~2
+-- query :: 'LuceneQuery'
+-- query = "foo" '=:' 'word' "bar" '~:' 2
+-- @
+--
+-- @
+-- -- foo:"bar baz qux"~10
+-- query :: 'LuceneQuery'
+-- query = "foo" '=:' 'phrase' ["bar", "baz", "qux"] '~:' 10
+-- @
+--
 (~:) :: FuzzableType a => LuceneExpr a -> Int -> LuceneExpr (TFuzzed a)
 (~:) = EFuzzy
 
+-- | Short-hand for fuzzing a word by 2. This is the default behavior of a @~@
+-- suffix without an integer added.
+--
+-- @
+-- -- foo:bar~
+-- query :: 'LuceneQuery'
+-- query = "foo" '=:' 'fuzzy' "bar" -- equivalent to: 'word' "bar" '~:' 2
+-- @
 fuzzy :: LuceneExpr TWord -> LuceneExpr TFuzzyWord
 fuzzy e = e ~: 2
 
+-- | A range expression.
+--
+-- @
+-- -- foo:[5 TO 10}
+-- query :: 'LuceneQuery'
+-- query = 'range' ('incl' ('int' 5)) ('excl' ('int' 10))
+-- @
 range :: PrimType a => Ranged (LuceneExpr a) -> Ranged (LuceneExpr a) -> LuceneExpr TRange
 range = ERange
 
+-- | An alias for 'range', to be used as an infix operator.
+--
+-- This will have one of the following two types:
+--
+-- @
+-- range :: 'Ranged' ('LuceneExpr' 'TWord') -> 'Ranged' ('LuceneExpr' 'TWord') -> 'LuceneExpr' 'TRange'
+-- range :: 'Ranged' ('LuceneExpr' 'TInt')  -> 'Ranged' ('LuceneExpr' 'TInt')  -> 'LuceneExpr' 'TRange'
+-- @
+--
+-- @
+-- -- foo:[5 TO 10}
+-- query :: 'LuceneQuery'
+-- query = 'range' ('incl' ('int' 5)) ('excl' ('int' 10))
+-- @
 to :: PrimType a => Ranged (LuceneExpr a) -> Ranged (LuceneExpr a) -> LuceneExpr TRange
 to = ERange
 
+-- | Mark an expression as inclusive, for use in a range query.
 incl :: LuceneExpr a -> Ranged (LuceneExpr a)
 incl = Inclusive
 
+-- | Mark an expression as exclusive, for use in a range query.
 excl :: LuceneExpr a -> Ranged (LuceneExpr a)
 excl = Exclusive
 
@@ -159,24 +265,23 @@ boost = EBoost
 (^:) = EBoost
 
 
-compileLuceneQuery :: LuceneExpr a -> ByteString
-compileLuceneQuery = BS.toLazyByteString . go
+-- TODO: escape characters
+compileLuceneExpr :: LuceneExpr a -> Builder
+compileLuceneExpr (EInt i) = BS.lazyByteString (BS.pack (show i))
+compileLuceneExpr ETrue = "true"
+compileLuceneExpr EFalse = "false"
+compileLuceneExpr (EWord s) = T.encodeUtf8Builder s
+compileLuceneExpr (EWild s) = T.encodeUtf8Builder s
+compileLuceneExpr (ERegex s) = "/" <> T.encodeUtf8Builder s <> "/"
+compileLuceneExpr (EPhrase ss) = foldMap compileLuceneExpr ss
+compileLuceneExpr (EFuzzy e n) = compileLuceneExpr e <> "~" <> BS.lazyByteString (BS.pack (show n))
+compileLuceneExpr (ERange r1 r2) =
+  case (r1, r2) of
+    (Inclusive e1, Inclusive e2) -> go '[' ']' e1 e2
+    (Inclusive e1, Exclusive e2) -> go '[' '}' e1 e2
+    (Exclusive e1, Inclusive e2) -> go '{' ']' e1 e2
+    (Exclusive e1, Exclusive e2) -> go '{' '}' e1 e2
  where
-  go :: LuceneExpr a -> Builder
-  go (EInt i) = BS.lazyByteString (BS.pack (show i))
-  go ETrue = "true"
-  go EFalse = "false"
-  go (EWord s) = T.encodeUtf8Builder s
-  go (EWild s) = T.encodeUtf8Builder s
-  go (ERegex s) = "/" <> T.encodeUtf8Builder s <> "/"
-  go (EPhrase ss) = foldMap go ss
-  go (EFuzzy e n) = go e <> "~" <> BS.lazyByteString (BS.pack (show n))
-  go (ERange r1 r2) =
-    case (r1, r2) of
-      (Inclusive e1, Inclusive e2) -> go' '[' ']' e1 e2
-      (Inclusive e1, Exclusive e2) -> go' '[' '}' e1 e2
-      (Exclusive e1, Inclusive e2) -> go' '{' ']' e1 e2
-      (Exclusive e1, Exclusive e2) -> go' '{' '}' e1 e2
-   where
-    go' :: Char -> Char -> LuceneExpr a -> LuceneExpr a -> Builder
-    go' c1 c2 e1 e2 = BS.char8 c1 <> go e1 <> " TO " <> go e2 <> BS.char8 c2
+  go :: Char -> Char -> LuceneExpr a -> LuceneExpr a -> Builder
+  go c1 c2 e1 e2 =
+    BS.char8 c1 <> compileLuceneExpr e1 <> " TO " <> compileLuceneExpr e2 <> BS.char8 c2
--- src/Lucene/Query/Typed.hs
+++ src/Lucene/Query/Typed.hs
@@ -1,8 +1,48 @@
 {-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE OverloadedStrings         #-}
 
-module Lucene.Query.Typed where
+module Lucene.Query.Typed
+  ( LuceneQuery
+  , (=:), (&&:), (||:), (-:)
+  , compileLuceneQuery
+  ) where
 
 import Lucene.Expr.Typed
 
+import Data.ByteString.Builder    (Builder)
+import Data.ByteString.Lazy.Char8 (ByteString)
+import Data.Monoid
+import Data.Text                  (Text)
+
+import qualified Data.Text.Encoding as T
+import qualified Data.ByteString.Builder as BS
+
+
 data LuceneQuery
-  = forall a. QExpr (LuceneExpr a)
+  = forall a. QField Text (LuceneExpr a)
+  | QAnd LuceneQuery LuceneQuery
+  | QOr LuceneQuery LuceneQuery
+  | QNot LuceneQuery LuceneQuery
+
+
+(=:) :: Text -> LuceneExpr a -> LuceneQuery
+(=:) = QField
+
+(&&:) :: LuceneQuery -> LuceneQuery -> LuceneQuery
+(&&:) = QAnd
+
+(||:) :: LuceneQuery -> LuceneQuery -> LuceneQuery
+(||:) = QOr
+
+(-:) :: LuceneQuery -> LuceneQuery -> LuceneQuery
+(-:) = QNot
+
+
+compileLuceneQuery :: LuceneQuery -> ByteString
+compileLuceneQuery = BS.toLazyByteString . go
+ where
+  go :: LuceneQuery -> Builder
+  go (QField f e) = T.encodeUtf8Builder f <> ":" <> compileLuceneExpr e
+  go (QAnd q1 q2) = "(" <> go q1 <> " AND " <> go q2 <> ")"
+  go (QOr q1 q2) = "(" <> go q1 <> " OR " <> go q2 <> ")"
+  go (QNot q1 q2) = "(" <> go q1 <> " NOT " <> go q2 <> ")"
