f289d0389a14ea89307d2457f5829a5210146cd5
Mitchell Rosen <mitchellwrosen@gmail.com>
2016-11-15 15:18:17 -0500

    Add custom type errors, remove should-not-typecheck test suite (too brittle)

--- src/Solr/Type.hs
+++ src/Solr/Type.hs
@@ -1,3 +1,7 @@
+{-# language CPP                                #-}
+{-# language UndecidableInstances               #-}
+{-# options_ghc -fno-warn-redundant-constraints #-}
+
 module Solr.Type
   ( -- * Solr types
     SolrType(..)
@@ -6,6 +10,9 @@ module Solr.Type
   , Rangeable
   ) where
 
+#if MIN_VERSION_base(4,9,0)
+import GHC.TypeLits (TypeError, ErrorMessage(..))
+#endif
 
 data SolrType
   = TNum
@@ -34,3 +41,31 @@ class Rangeable (ty :: SolrType)
 instance Rangeable 'TNum
 instance Rangeable 'TWord
 instance Rangeable 'TDateTime
+
+#if MIN_VERSION_base(4,9,0)
+instance TypeError ('Text "You can only fuzz words and phrases") => Fuzzable 'TNum
+instance TypeError ('Text "You can only fuzz words and phrases") => Fuzzable 'TBool
+instance TypeError ('Text "You can only fuzz words and phrases") => Fuzzable 'TWild
+instance TypeError ('Text "You can only fuzz words and phrases") => Fuzzable 'TRegex
+instance TypeError ('Text "You can only fuzz words and phrases") => Fuzzable 'TDateTime
+instance TypeError ('Text "You can only fuzz words and phrases") => Fuzzable ('TFuzzed ty)
+instance TypeError ('Text "You can only fuzz words and phrases") => Fuzzable ('TBoosted ty)
+instance TypeError ('Text "You can only fuzz words and phrases") => Fuzzable ('TRanged ty)
+
+instance TypeError ('Text "You can only boost words and phrases") => Boostable 'TNum
+instance TypeError ('Text "You can only boost words and phrases") => Boostable 'TBool
+instance TypeError ('Text "You can only boost words and phrases") => Boostable 'TWild
+instance TypeError ('Text "You can only boost words and phrases") => Boostable 'TRegex
+instance TypeError ('Text "You can only boost words and phrases") => Boostable 'TDateTime
+instance TypeError ('Text "You can only boost words and phrases") => Boostable ('TFuzzed ty)
+instance TypeError ('Text "You can only boost words and phrases") => Boostable ('TBoosted ty)
+instance TypeError ('Text "You can only boost words and phrases") => Boostable ('TRanged ty)
+
+instance TypeError ('Text "You can only use numbers, words, and dates in a range expression") => Rangeable 'TBool
+instance TypeError ('Text "You can only use numbers, words, and dates in a range expression") => Rangeable 'TWild
+instance TypeError ('Text "You can only use numbers, words, and dates in a range expression") => Rangeable 'TRegex
+instance TypeError ('Text "You can only use numbers, words, and dates in a range expression") => Rangeable 'TPhrase
+instance TypeError ('Text "You can only use numbers, words, and dates in a range expression") => Rangeable ('TFuzzed ty)
+instance TypeError ('Text "You can only use numbers, words, and dates in a range expression") => Rangeable ('TBoosted ty)
+instance TypeError ('Text "You can only use numbers, words, and dates in a range expression") => Rangeable ('TRanged ty)
+#endif
